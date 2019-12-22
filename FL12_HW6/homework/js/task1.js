13498fd02daf48667e97c5bb018cdd19d173e779
maxrusmr2011@gmail.com <maxrusmr2011@gmail.com>
2019-12-22 18:06:11 +0200

    '22.12.19'

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -11,11 +11,11 @@ if(isNaN(varA)||isNaN(varB)||isNaN(varB)||varA === zero) {
 		console.log('no solution (Discriminant < 0)');
 	} else if (varD === zero) {
 		const varResult1 = -varB / (num2 * varA);
-		console.log('x = ' + varResult1);
+		console.log('x = ' + Math.round(varResult1));
 	} else {
 		const varResult1 = -(varB + Math.sqrt(varD)) / num2 * varA,
 		varResult2 = -(varB - Math.sqrt(varD)) / (num2 * varA);
-		console.log('x1 = ' + varResult1 + ' and x2 = ' + varResult2);
+		console.log('x1 = ' + Math.round(varResult1) + ' and x2 = ' + Math.round(varResult2));
 	}				
 }
 
