9b7c8fadcf76c2b652aeb25fb7b3e61146fb6a83
maxrusmr2011@gmail.com <maxrusmr2011@gmail.com>
2019-12-23 22:41:32 +0200

    '23.12.19'

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,21 +1,30 @@
 let varA, varB, varC, varD;
 const zero=0, num2=2, num4=4;
-varA = Number( prompt('Input variable A') );
-varB = Number( prompt('Input variable B') );
-varC = Number( prompt('Input variable C') );
-if(isNaN(varA)||isNaN(varB)||isNaN(varB)||varA === zero) {
+varA = prompt('Input variable A');
+varB = prompt('Input variable B');
+varC = prompt('Input variable C');
+varA=varA?varA.trim():varA;
+varB=varB?varB.trim():varB;
+varC=varC?varC.trim():varC;
+if(!varA||!varB||!varC){
 	console.log('Invalid input data');
 }else{
-	varD = varB * varB - num4 * varA * varC;			
-	if (varD < zero) {
-		console.log('no solution (Discriminant < 0)');
-	} else if (varD === zero) {
-		const varResult1 = -varB / (num2 * varA);
-		console.log('x = ' + Math.round(varResult1));
-	} else {
-		const varResult1 = -(varB + Math.sqrt(varD)) / num2 * varA,
-		varResult2 = -(varB - Math.sqrt(varD)) / (num2 * varA);
-		console.log('x1 = ' + Math.round(varResult1) + ' and x2 = ' + Math.round(varResult2));
-	}				
+	varA=Number(varA);
+	varB=Number(varB);
+	varC=Number(varC);
+	if(isNaN(varA)||isNaN(varB)||isNaN(varB)||!isFinite(varA)||!isFinite(varB)||!isFinite(varC)||varA === zero) {
+		console.log('Invalid input data');
+	}else{
+		varD = varB * varB - num4 * varA * varC;			
+		if (varD < zero) {
+			console.log('no solution');
+		} else if (varD === zero) {
+			const varResult1 = -varB / (num2 * varA);
+			console.log('x = ' + Math.round(varResult1));
+		} else {
+			const varResult1 = -(varB + Math.sqrt(varD)) / num2 * varA,
+			varResult2 = -(varB - Math.sqrt(varD)) / (num2 * varA);
+			console.log('x1 = ' + Math.round(varResult1) + ' and x2 = ' + Math.round(varResult2));
+		}				
+	}
 }
-
--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,27 +1,29 @@
 let varA, varB, varC;
-const zero=0, num2=2;
+const zero=0;
 varA = prompt('Input length for triangle side A');
 varB = prompt('Input length for triangle side B');
 varC = prompt('Input length for triangle side C');
-if(!varA||!varB||!varC||isNaN(Number(varA))||isNaN(Number(varB))||isNaN(Number(varC)) ){
+varA=varA?varA.trim():varA;
+varB=varB?varB.trim():varB;
+varC=varC?varC.trim():varC;
+if(!varA||!varB||!varC){
 	alert('input values should be ONLY numbers');
 }else{
 	varA=Number(varA);
 	varB=Number(varB);
-	varC=Number(varC);
-	if(varA<=zero||varB<=zero||varC<=zero){
+	varC=Number(varC);	
+	if(isNaN(varA)||isNaN(varB)||isNaN(varC)||!isFinite(varA)||!isFinite(varB)||!isFinite(varC) ){
+		alert('input values should be ONLY numbers');
+	}else if(varA<=zero||varB<=zero||varC<=zero){
 		alert('A triangle must have 3 sides with a positive definite length ');
-	}else{
-		if(varA>=varB+varC||varB>=varA+varC||varC>=varB+varA){
-			alert('Triangle doesn\'t exist');
-		}else{
-			if (varA===varB&&varA===varC) {
-				console.log('Equilateral triangle');
-			} else if(varA===varB||varA===varC||varC===varB){
-				console.log('Isosceles triangle');
-			} else {
-				console.log('Scalene triangle');
-			}
-		}
+	}else if(varA>=varB+varC||varB>=varA+varC||varC>=varB+varA){
+		alert('Triangle doesn\'t exist');
+	}else if (varA===varB&&varA===varC) {
+		console.log('Equilateral triangle');
+	}else if(varA===varB||varA===varC||varC===varB){
+		console.log('Isosceles triangle');
+	}else {
+		console.log('Scalene triangle');
 	}
+	
 }
