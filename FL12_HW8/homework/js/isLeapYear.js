c1e747f6727ece3fe7ebdd97b8760635f4ff2c5f
maxrusmr2011@gmail.com <maxrusmr2011@gmail.com>
2019-12-31 17:02:31 +0200

    '31.12.19'

--- FL12_HW8/homework/js/isLeapYear.js
+++ FL12_HW8/homework/js/isLeapYear.js
@@ -4,14 +4,10 @@ function isLeapYear (date) {
 		year=(new Date(date)).getFullYear();
 	}
 	if (year) {
-		if (year%400 === 0) {
-			result = year +' is a leap year';
-		} else if (year%100 === 0) {
-			result = year+' is not a leap year';
-		} else if (year%4 === 0) {
-			result = year +' is a leap year';
+		if (year%4 === 0 && year%100 !== 0 || year%400 === 0) {
+			result = `${year} is a leap year`;
 		} else {
-			result = year+' is not a leap year';
+			result = `${year} is not a leap year`;
 		}
 	}
 	return result;
